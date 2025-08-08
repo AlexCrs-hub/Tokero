ce64b3145c2d94c79203332fc7c244de4bfc1918
AlexCrs-hub <achi.grigore@gmail.com>
2025-08-08 20:41:58 +0300

    added db configuration and crypto coin table

--- TokeroApp/TokeroApp/App.xaml.cs
+++ TokeroApp/TokeroApp/App.xaml.cs
@@ -1,10 +1,15 @@
-﻿namespace TokeroApp
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using TokeroApp.Services;
+
+namespace TokeroApp
 {
     public partial class App : Application
     {
+        public static LocalDbService Database { get; private set; }
         public App()
         {
             InitializeComponent();
+            Database = new LocalDbService();
 
             MainPage = new AppShell();
         }
---
+++ TokeroApp/TokeroApp/Services/LocalDbService.cs
@@ -0,0 +1,39 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TokeroApp.Model;
+
+namespace TokeroApp.Services
+{
+    public class LocalDbService
+    {
+        private const string DB_NAME = "crypto_db.db3";
+        private readonly SQLiteAsyncConnection _connection;
+
+        public LocalDbService()
+        {
+            _connection = new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, DB_NAME));
+            _connection.CreateTableAsync<CryptoCoin>();
+        }
+
+        public async Task<List<CryptoCoin>> GetCryptoCoins()
+        {
+            return await _connection.Table<CryptoCoin>().ToListAsync();
+        }
+
+        public async Task<List<CryptoCoin>> GetBySymbol(string symbol)
+        {
+            return await _connection.Table<CryptoCoin>()
+                .Where(p => p.Symbol == symbol)
+                .ToListAsync();
+        }
+
+        public async Task Create(CryptoCoin coin)
+        {
+            await _connection.InsertAsync(coin);
+        }
+    }
+}
